d0cb867e268a340fea78fb71f451ab0a08383f4b
Michael Elovskikh <wronglink@gmail.com>
2013-04-25 14:33:31 +0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='mstranslator',
+    version='0.1',
+    license='MIT',
+    description='Microsoft Translator Api wrapper',
+    author='Michael Elovskikh',
+    author_email='wronglink@gmail.com',
+    py_modules=['mstranslator'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ),
+)
