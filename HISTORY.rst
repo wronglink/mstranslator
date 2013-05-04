af96b49781d9357071d6cd618c548175b4fcf7e7
Michael Elovskikh <wronglink@gmail.com>
2013-05-04 19:40:22 +0600

    Added HISTORY.rst, increased package version

---
+++ HISTORY.rst
@@ -0,0 +1,7 @@
+History
+-------
+
+0.2.1 (2013-05-04)
+++++++++++++++++++
+
+- Added get_lang_names method
--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup
 
 setup(
     name='mstranslator',
-    version='0.2',
+    version='0.2.1',
     license='MIT',
     url='https://github.com/wronglink/mstranslator',
     description='Microsoft Translator API wrapper',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
     author='Michael Elovskikh',
     author_email='wronglink@gmail.com',
     py_modules=['mstranslator'],
